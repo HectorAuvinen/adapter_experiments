ffae9b0939388b5fc69de8bb0788e111203987d4
Hector Auvinen <hectorauvinen@gmail.com>
2024-01-29 00:39:09 +0200

    first module

---
+++ src/data_handling/load_data.py
@@ -0,0 +1,36 @@
+from datasets import load_dataset,DatasetDict
+import logging
+
+GLUE_TASKS = ['ax', 'cola', 'mnli', 'mnli_matched', 'mnli_mismatched', 'mrpc', 'qnli', 'qqp', 'rte', 'sst2', 'stsb', 'wnli']
+SUPER_GLUE_TASKS = ['axb', 'axg', 'boolq', 'cb', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc', 'wsc.fixed']
+
+
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level as needed
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filename="my_log_file.log"  # Specify the log file name
+)
+
+
+def load_hf_dataset(task_name:str,
+                    debug:bool=False) -> DatasetDict:
+    
+    if task_name in GLUE_TASKS:
+        dataset = load_dataset("glue",task_name)
+    elif task_name in SUPER_GLUE_TASKS:
+        dataset = load_dataset("super_glue",task_name)
+    else:
+        dataset = load_dataset(task_name)
+    
+    if debug:
+        selected_datasets = {split: dataset[split].select(range(100)) for split in dataset.keys()}
+        # Convert the dictionary back into a DatasetDict
+        dataset = DatasetDict(selected_datasets)
+    
+    # Use the logger to replace print statements
+    logging.info(f"Number of rows: {dataset.num_rows}")
+    logging.info(f"Features for 'train' split: {dataset['train'].features}")
+    logging.info(f"Sample from 'train' split: {dataset['train'][0]}")
+    
+    return dataset
+    
---
+++ src/data_handling/utils.py
@@ -0,0 +1,26 @@
+def process_labels(labels):
+    """
+    Converts labels to integers and performs zero indexing if necessary.
+    If all labels are already 0 or 1, no conversion is performed.
+    
+    Args:
+    labels (List[Union[int, str]]): List of labels where each label can be an integer or a string.
+    
+    Returns:
+    List[int]: Processed labels as integers with zero indexing.
+    """
+    # Check if all labels are 0 or 1
+    all_01 = all(label in (0, 1) for label in labels)
+
+    processed_labels = []
+    for label in labels:
+        if all_01:
+            processed_labels.append(label)  # Leave labels as they are
+        elif isinstance(label, int):
+            processed_labels.append(label - 1)  # Convert to zero indexing
+        elif isinstance(label, str) and label.isdigit():
+            processed_labels.append(int(label) - 1)  # Convert to int and zero indexing
+        else:
+            processed_labels.append(0)  # Default to zero indexing if not recognized
+    
+    return processed_labels
