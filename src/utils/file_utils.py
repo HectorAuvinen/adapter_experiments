ecb8dd2896ce11543faf3507a100ffe58ebeb270
Hector Auvinen <hectorauvinen@gmail.com>
2024-03-04 13:43:24 +0200

    add plotting utils

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -1,6 +1,7 @@
 # import
 import os
 import json
+from pathlib import Path
 
 def json_to_dict(file_path):
     with open(file_path, "r") as json_file:
@@ -28,3 +29,25 @@ def write_eval_results(eval_results,output_dir,task,trainer,adapter_config,batch
                 writer.write("%s = %s\n" % (key,value))
     
     
+    
+def read_eval_results(path):
+    res_path = Path(path)
+    trainingtime = 0
+    new_results = {}
+    for config in res_path.iterdir():
+        config_results = {config.name:{}}
+        for seed in config.iterdir():
+            config_results[config.name][seed.name] = {}
+            for dataset in seed.iterdir():
+                task = dataset.name.split("eval_results_")[-1].split(".txt")[0]
+                try:
+                    with open(dataset,"r") as file:
+                        lines = file.readlines()
+                        time = [float(detail.split("=")[1].strip()) for detail in lines if "training time" in detail][0]
+                        trainingtime += time
+                        line = [float(line.strip().split("= ")[-1]) for line in lines if "eval_accuracy" in line][0]
+                        config_results[config.name][seed.name][task] = line
+                except PermissionError as e:
+                    continue
+        new_results.update(config_results)
+    return new_results
--- src/utils/plot_utils.py
+++ src/utils/plot_utils.py
@@ -1,2 +1,80 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Get the absolute path to the root of the project
+project_root = os.path.dirname(script_dir)
+# Add the project root to the sys.path
+sys.path.insert(0, project_root)
+
+
+from src.constants.constants import DATASET_SIZES
+
+
+def prepare_eval_for_plots(new_results):
+    # Calculate mean and standard deviation for accuracies
+    mean_accuracies = {}
+    std_dev_accuracies = {}  # Store standard deviations here
+
+    for reduction_factor, seeds in new_results.items():
+        for seed, tasks in seeds.items():
+            for task, accuracy in tasks.items():
+                if task not in mean_accuracies:
+                    mean_accuracies[task] = {}
+                    std_dev_accuracies[task] = {}  # Initialize std deviation dict
+                if reduction_factor not in mean_accuracies[task]:
+                    mean_accuracies[task][reduction_factor] = []
+                    std_dev_accuracies[task][reduction_factor] = []  # Initialize list for std deviation values
+                mean_accuracies[task][reduction_factor].append(accuracy)
+
+    for task, reduction_factors in mean_accuracies.items():
+        for reduction_factor, accuracies in reduction_factors.items():
+            mean_accuracies[task][reduction_factor] = np.mean(accuracies)
+            std_dev_accuracies[task][reduction_factor] = np.std(accuracies)  # Calculate standard deviation
+
+    # Sort and prepare data for plotting
+    sorted_reduction_factors = sorted(new_results.keys(), key=lambda x: (x != 'full_fine_tune', int(x.split('_')[-1]) if x != 'full_fine_tune' else -1))
+    """    DATASET_SIZES = {'mnli':392702, 'qqp':363849, 'sst2':67349, 'winogrande':40398, 'imdb':25000, 'hellaswag':39905,
+                    'social_i_qa':33410, 'cosmos_qa':25262, 'scitail':23097, 'argument':18341,
+                    'commonsense_qa':9741, 'boolq':9427, 'mrpc':3668, 'sick':4439, 'rte':2490, 'cb':250}
+    """
+    tasks = sorted([task for task in mean_accuracies.keys() if task in DATASET_SIZES], key=lambda task: DATASET_SIZES[task], reverse=True)
+
+    data = {reduction_factor: [] for reduction_factor in sorted_reduction_factors}
+    errors = {reduction_factor: [] for reduction_factor in sorted_reduction_factors}  # For standard deviations
+    for task in tasks:
+        for reduction_factor in sorted_reduction_factors:
+            data[reduction_factor].append(mean_accuracies[task].get(reduction_factor, np.nan))
+            errors[reduction_factor].append(std_dev_accuracies[task].get(reduction_factor, np.nan))
+    
+    return data,errors,sorted_reduction_factors,tasks
+
+def plot_evaluation(data,errors,sorted_reduction_factors,tasks):
+    plt.figure(figsize=(12, 8))
+    num_reduction_factors = len(sorted_reduction_factors)
+    bar_width = 0.8 / num_reduction_factors
+    #
+    error_kw = {
+    #'capsize': 10,  # Adjust cap size if needed
+    #'elinewidth': bar_width / 2,  # Adjust line width to match bar width as closely as possible
+    #'ecolor': 'lightgrey',  # Set error bar color to grey
+    #'alpha': 0.7,  # Adjust transparency of the error bars
+    }
+    #
+    for i, reduction_factor in enumerate(sorted_reduction_factors):
+        positions = np.arange(len(tasks)) + i * bar_width
+        plt.bar(positions, data[reduction_factor], width=bar_width, alpha=0.8, label=reduction_factor, 
+                yerr=errors[reduction_factor], error_kw=error_kw,capsize=5)
+
+    plt.yticks(np.arange(0, 1.05, 0.05))
+    plt.xlabel('Task')
+    plt.ylabel('Mean Accuracy')
+    plt.title('Mean Accuracy by Task and Reduction Factor')
+    labels = [f"{task}\n{DATASET_SIZES[task]} training samples" for task in tasks]
+    plt.xticks(np.arange(len(tasks)) + bar_width * (len(sorted_reduction_factors) - 1) / 2, labels)
+    plt.legend(title="Reduction Factor", loc="lower right")
+    plt.tight_layout()
+    plt.show()
