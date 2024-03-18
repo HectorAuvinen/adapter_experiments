66a585936f1ef76c8e572681111afd2e3a101c7c
Hector Auvinen <hectorauvinen@gmail.com>
2024-03-18 11:23:35 +0200

    some stuff

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -30,24 +30,52 @@ def write_eval_results(eval_results,output_dir,task,trainer,adapter_config,batch
     
     
     
-def read_eval_results(path):
+def read_eval_results(path,two_datasets=False):
     res_path = Path(path)
     trainingtime = 0
     new_results = {}
+    batch_sizes = {}
+    max_lengths = {}
     for config in res_path.iterdir():
         config_results = {config.name:{}}
         for seed in config.iterdir():
             config_results[config.name][seed.name] = {}
             for dataset in seed.iterdir():
                 task = dataset.name.split("eval_results_")[-1].split(".txt")[0]
+                if two_datasets:
+                    #print(task)
+                    if task not in ["sst2","sick"]:
+                        #print("skipping task",task)
+                        continue
                 try:
                     with open(dataset,"r") as file:
                         lines = file.readlines()
+                        #
+                        current_batch_size = int([line.split("=")[1].strip() for line in lines if "batch size" in line][0])
+                        current_max_length = int([line.split("=")[1].strip() for line in lines if "max length" in line][0])
+                        
+                        if task in batch_sizes and max_lengths:
+                            batch_sizes[task].append(current_batch_size)
+                            max_lengths[task].append(current_max_length)
+                        else:
+                            batch_sizes[task] = [current_batch_size]
+                            max_lengths[task] = [current_max_length]
+                        #if batch_size is not None and max_length is not None:
+                        #    assert current_batch_size == batch_size, f"Batch size mismatch: expected {batch_size}, got {current_batch_size}"
+                        #    assert current_max_length == max_length, f"Max length mismatch: expected {max_length}, got {current_max_length}"
+                        #else:
+                        #batch_size = current_batch_size
+                        #max_length = current_max_length
+                        #
                         time = [float(detail.split("=")[1].strip()) for detail in lines if "training time" in detail][0]
                         trainingtime += time
-                        line = [float(line.strip().split("= ")[-1]) for line in lines if "eval_accuracy" in line][0]
-                        config_results[config.name][seed.name][task] = line
+                        accuracy = [float(line.strip().split("= ")[-1]) for line in lines if "eval_accuracy" in line][0]
+                        #config_results[config.name][seed.name][task] = {'accuracy': accuracy, 'batch_size': batch_size, 'max_length': max_length}
+                        config_results[config.name][seed.name][task] = accuracy
                 except PermissionError as e:
                     continue
         new_results.update(config_results)
+        
+    print("batches",batch_sizes)
+    print("lengths",max_lengths)
     return new_results
